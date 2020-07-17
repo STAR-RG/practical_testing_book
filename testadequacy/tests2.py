0862112a5ffdf9eb3bacbb2cb564c420ceab9978
Galileu Santos <galilasmb@gmail.com>
2020-07-17 01:23:21 -0300

    Updating coverage and tests

---
+++ testadequacy/tests2.py
@@ -0,0 +1,20 @@
+#tests2.py
+from math import pi
+import unittest
+from area import *
+
+class TestCircleArea(unittest.TestCase): 
+    def test_area(self):
+        #Test area when radius >= 0
+        self.assertAlmostEqual(circle_area(1), pi)
+        self.assertAlmostEqual(circle_area(0), 0)
+        self.assertAlmostEqual(circle_area(2.1), pi * 2.1**2)
+
+    def test_values(self):
+        #Make sure value errors are raised when necessary
+        self.assertRaises(ValueError, circle_area, -2)
+
+    def test_type(self):
+        #Make sure type errors are raised when necessary
+        self.assertRaises(TypeError, circle_area, 3+5j)
+        self.assertRaises(TypeError, circle_area, True)
